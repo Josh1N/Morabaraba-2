60f60884946a8a921bff55c5b77592ed8e7ee993
Josh1N <jayngwarai@gmail.com>
2018-04-06 00:44:53 +0200

    Implementing board drawing

---
+++ Morabaraba-2/GameBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morabaraba_2
+{
+    //class GameBoard
+    //{
+    //    // public List<Cell> Cells { get; set; }
+
+    //    //public GameBoard
+    //    //    {
+
+    //    //    }
+
+    //}
+
+}
--- Morabaraba-2/Program.cs
+++ Morabaraba-2/Program.cs
@@ -27,6 +27,12 @@ namespace Morabaraba_2
    *      -> if their OnBoard is less than 3, the other player has won.
    *         
    */
+
+        public string[] Positions = { "A1", "A4", "A7", "B2","B4","B6", "C3","C4","C5","D1","D2","D3", "D5", "D6", "D7","E3","E4","E5","F2","F4","F6","G1","G4","G7" };
+
+        public List<string> UnplacedCows  = new List<string>(); //NEED TO KNOW WHETHER TO MAKE TWO LISTS (ONE) FOR EACH PLAYER SO AS TO KNOW HOW TO KEEP TO TRACK OF WHO HAS WHICH COWS
+        public List<string> onBoardCows = new List<string>();
+
         static void runGame()//player currentPlayer) // an object of type player needs to be created
         {
 
@@ -46,40 +52,41 @@ namespace Morabaraba_2
             Console.Write("Morabaraba Game Instructions\n\nHOW TO START\n1. 	To start you need the Twelve men's morris game board. \n	The board starts empty, each player holding all his pieces \n	in hand.\n2. 	Each player has 12 game pieces. One player plays white, \n	the other black, but any 2 differing colors can be used.\n\nRULES\n1. 	The gameboard for 12 men's morris.\n2. 	When a player is reduced to 4 pieces, his pieces are free \n	to move to any unoccupied point, instead of being restricted \n	to adjacent points as earlier in the game.\n\nGAMEPLAY\n1. 	At first, each player in turn puts one piece on the board, \n	at any vacant point.\n2. 	Once all pieces are on the board, a player now moves one of \n	his pieces along a marked line to an adjacent empty point.\n3. 	If a piece placed or moved forms a row of three along a marked\n 	line (This is called a mill), he can take one of his opponent's \n	pieces, as long as that piece is not itself part of a mill.\n4. 	If when capturing, all opposing pieces have formed mills, then\n 	any of the pieces may be captured.\n\nHOW TO WIN\n1. 	The goal of the game is to reduce your opponent's pieces to as\n 	little as possible.\n2. 	A player wins the game when his opponent is reduced to 2 pieces \n	and is thus unable to form a mill or make any further captures.\n3. 	If the board is filled in the first phase, and no pieces taken, \n	the second phase will be gridlocked with neither player able to move.\n 	In this case the game is draw.\n\n");
         }
 
-       
-
-       // static List<Position> Cowp1 = new  List <Position>();
 
 
-        List<string> rows = new List<string>() // need to figure out how to circumanvigate escape character 
+        private List<string> rows = new List<string>() // need to figure out how to circumanvigate escape character 
         {
-            "  1  2  3  4  5  6  7  \n",
-            "A O--------O--------O  \n",
+             "  1  2  3  4  5  6  7  \n",
+            string.Format("A {0}--------{1}--------{2}  \n","A1","A4","A7"),
             "  | '      |       |  \n",
-            "B |  O-----O-----O  |  \n",
+            string.Format("B |  {O}-----{1}-----{2}  |  \n","B2","B4","B6"),
             "  |  | '   |   / |  |  \n",
-            "C |  |  O--O--O  |  |  \n",
+            string.Format("C |  |  {O}--{1}--{2}  |  |  \n","C3","C4","C5"),
             "  |  |  |     |  |  |  \n",
-            "D O--O--O     O--O--O  \n",
+            string.Format("D {0}--{1}--{2}     {3}--{4}--{5}  \n","D1","D2","D3", "D5", "D6", "D7"),
             "  |  |  |     |  |  |  \n",
-            "E |  |  O--O--O  |  |  \n",
+            string.Format("E |  |  {0}--{1}--{2}  |  |  \n","E3","E4","E5"),
             "  |  | /   |   ' |  |  \n",
-            "F |  O-----O-----O  |  \n",
+            string.Format("F |  {0}-----{1}-----{2}  |  \n","F2","F4","F6"),
             "  | /      |      ' |  \n",
-            "G O--------O--------O  \n\n"
+            string.Format("G {0}--------{1}--------{2}  \n\n","G1","G4","G7")
         };
 
 
-        static void printGameBoard(List<string> rows)
+        public static void printGameBoard(List<string> var)
         {
             // prints list of strings for each row of the board
             // strings in the list can be updated via their index whenever the board changes 
             // this is going to be similar to what we tried to do in F# but it'll be alot easier because
             // its just a matter of indexing and updating at that index 
-            foreach (string r in rows)
+            foreach (string r in var)
             {
                 Console.Write(r);
             }
+
+
+        
+
         }
 
         static void Main(string[] args)
@@ -87,6 +94,7 @@ namespace Morabaraba_2
             printInstructions();
             Console.WriteLine("Press Enter to begin the game:");
             Console.ReadLine();
+            printGameBoard(rows);
            // runGame(player currentPlayer); // an object of type player needs to be created 
             // run game is where everything should happen
             // should take 
